457ba499024f2aa2d56d8edc70baab4b53d2b1a0
Yusuf Senturk <yusufsengs8@gmail.com>
2024-06-01 14:29:47 +0300

    basics of kotlin variables(int,string,boolean)

--- app/src/main/java/com/example/kotlinlearning/main.kt
+++ app/src/main/java/com/example/kotlinlearning/main.kt
@@ -1,6 +1,32 @@
 package com.example.kotlinlearning
 
 fun main(){
-    println("yusuf")
-    println("Hello world")
+//    println("yusuf")
+//    println("Hello world")
+//   ------------ Değişkenler--------------
+//    var x=10//var can change
+//    val z=20//z cant change in other lines because of val
+//
+//    var ornek: Long =10//we can change varibale type like this
+//    ---------------- Strings-------------
+//    var userName="Yusuf"
+//    var userLastName="Şentürk"
+//    var reversFullName=""
+//
+//    for(letter in userName+userLastName){
+//        reversFullName=letter+reversFullName
+//    }
+//
+//    print(reversFullName.lowercase())
+//    --------------convertions var to var------------------
+//    val age = "37"
+//    val ageInt=age.toInt()
+//    print(age+"yusuf"+"---->"+ageInt*5)
+//    println("Hata var lan")
+
+
+
+
+
+
 }
